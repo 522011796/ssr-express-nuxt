c093cce8c9a9bff54c7f999b39d72db18914fca7
9bf_100006791283_sanguing <9bf_100006791283_sanguing@git.cloud.tencent.com>
2018-11-22 18:13:05 +0800

    新增：工具类，登录权限验证，修改部分中间件问题

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const express = require('express')
 const consola = require('consola')
 const { Nuxt, Builder } = require('nuxt')
 const app = express()
-const host = process.env.HOST || '127.0.0.1'
+const host = process.env.HOST || '0.0.0.0'
 const port = process.env.PORT || 3000
 
 app.set('port', port)
@@ -29,7 +29,8 @@ async function start() {
   app.use(nuxt.render)
 
   // Listen the server
-  app.listen(port, host)
+  //app.listen(port, host)
+  app.listen(port,host)
   consola.ready({
     message: `Server listening on http://${host}:${port}`,
     badge: true
